2fa0ea955544167c84609616a986b70a687bff84
Abi <abigail.carvajal@taotechideas.com>
2022-06-08 13:03:05 -0500

    generic class and execution

---
+++ Generic_Actions/Generic_Action.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Constraints: informa al compilador sobre las capacidades que debe tener un argumento de tipo.
+// Struct--> El argumento de tipo debe ser un tipo de valor que no admita valores NULL
+namespace Generic_Actions
+{
+    public class Generic_Action<T> where T : struct 
+    {
+        public T value1;
+        public T value2;
+
+        public Generic_Action(T value1, T value2)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+
+        public T add()
+        {
+            return (dynamic)value1 + (dynamic)value2;
+        }
+
+        public T susbtraction()
+        {
+            return (dynamic)value1 - (dynamic)value2;
+        }
+
+        public T multiply()
+        {
+            return (dynamic)value1 * (dynamic)value2;
+        }
+
+        public T div()
+        {
+            if((dynamic)value2 != 0)
+            {
+                return (dynamic)value1 / (dynamic)value2;
+            }
+            else
+            {
+                throw new ArithmeticException();
+            }
+        }
+
+        public string toString()
+        {
+            return "Suma = " + add() + " Resta = " + susbtraction() + " Multiplicacion = " + multiply() + " División:" + div();
+        }
+
+    }
+}
---
+++ Generic_Actions/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Generic_Actions;
+
+Generic_Action<double> actions_double = new Generic_Action<double>(1.55,5.00);
+Generic_Action<int> actions_int = new Generic_Action<int>(1, 5);
+Generic_Action<float> actions_float = new Generic_Action<float>(1.55f, 5.0f);
+
+
+//El tipo "string" debe ser un tipo que no acepte valores NULL --> no acepta valores String o clase persona da problema al querer instanciar 
+//Generic_Action<string> action_string = new Generic_Action<string>("A", "B");
+//Generic_Action<Person> action_class = new Generic_Action<Person>(new Person(),new Person());
+
+Console.WriteLine(actions_double.toString());
+Console.WriteLine(actions_int.toString());
+Console.WriteLine(actions_float.toString());
+
+
